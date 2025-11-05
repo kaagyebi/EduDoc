1568d877290b612579dd6e91f08a8e287f23f2b2
Bertojnr <snrdeveloper07@gmail.com>
2025-11-05 14:31:04 +0000

    new update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import connectDB from './config/db.js';
 dotenv.config();
     
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
